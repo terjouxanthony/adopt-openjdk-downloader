a3ed0ea07be05304994c6ac6b9e3650770e5f4ae
Anthony <anthony.terjoux@gmail.com>
2021-05-21 02:09:43 -0400

    Update maven-plugin javadoc and deployment config.

--- maven-plugin/src/main/java/io/github/terjouxanthony/jdk/downloader/maven/plugin/JdkDownloaderMojo.java
+++ maven-plugin/src/main/java/io/github/terjouxanthony/jdk/downloader/maven/plugin/JdkDownloaderMojo.java
@@ -19,6 +19,43 @@ import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.time.Duration;
 
+/**
+ * Downloads and installs a general availability hotspot JDK/JRE from AdoptOpenJDK vendor.
+ * <p>
+ * If the JDK/JRE is already present, no download is performed, except if the flag 'downloadLatest' is true and if there is a newer minor/bugfix version for the supplied java version.
+ * The JDK/JREs are installed by default under the folder $HOME/.m2/java. This can be configured by the rootDir parameter.
+ * <p>
+ * After execution, the plugin sets the following project properties:
+ * <ul>
+ *     <li><b>jdk-downloader-maven-plugin.jdk-install-path</b>: Absolute path to the folder containing the JDK/JRE</li>
+ *     <li><b>jdk-downloader-maven-plugin.jdk-home</b>: Absolute path to the JDK/JRE</li>
+ * </ul>
+ * The plugin executes in the PREPARE_PACKAGE lifecycle phase, so other plugins can use the above properties if they are bound to the PACKAGE phase.
+ * Example of properties usage: ${jdk-downloader-maven-plugin.jdk-home}.
+ * <p>
+ * Example of the plugin's usage:
+ * <pre>
+ * {@code
+ * <plugin>
+ *     <groupId>io.github.terjouxanthony</groupId>
+ *     <artifactId>jdk-downloader-maven-plugin</artifactId>
+ *     <version>0.0.1</version>
+ *     <executions>
+ *         <execution>
+ *             <goals>
+ *                 <goal>download-java</goal>
+ *             </goals>
+ *         </execution>
+ *     </executions>
+ *     <configuration>
+ *         <imageType>jre</imageType>
+ *         <os>windows</os>
+ *         <architecture>x64</architecture>
+ *     </configuration>
+ * </plugin>
+ * }
+ * </pre>
+ */
 @Mojo(name = "download-java", defaultPhase = LifecyclePhase.PREPARE_PACKAGE)
 public class JdkDownloaderMojo extends AbstractMojo {
 
@@ -27,29 +64,56 @@ public class JdkDownloaderMojo extends AbstractMojo {
 
     // Mandatory params:
 
+    /**
+     * Architecture, example: x64, x32, ppc64, arm
+     */
     @Parameter(property = "architecture", required = true)
     String architecture;
 
+    /**
+     * Operating system, example: linux, windows, mac, solaris
+     */
     @Parameter(property = "os", required = true)
     String os;
 
+    /**
+     * jre or jdk
+     */
     @Parameter(property = "imageType", required = true)
-    String imageType; // jdk or jre
+    String imageType;
 
     // Optional params:
 
-    @Parameter(property = "downloadLatest", defaultValue = "false", required = true)
+    /**
+     * Optional flag. If true, always request AdoptOpenJdk API to get the latest version with bugfixes.
+     * If there is a minor/bugfix version, it is downloaded alongside the previous ones. To clean them, use the flag cleanExistingSameMajorVersion.
+     */
+    @Parameter(property = "downloadLatest", defaultValue = "false")
     Boolean downloadLatest;
 
-    @Parameter(property = "cleanExistingSameMajorVersion", defaultValue = "true", required = true)
+    /**
+     * Optional flag. If true, delete the previously downloaded artifacts for the supplied java version.
+     */
+    @Parameter(property = "cleanExistingSameMajorVersion", defaultValue = "true")
     Boolean cleanExistingSameMajorVersion;
 
+    /**
+     * Optional value. Root folder to store JRE's and JDK's, defaults to $HOME/.m2/java
+     */
     @Parameter(property = "rootDir")
     String rootDir;  // root folder to store JRE's and JDK's, defaults to $HOME/.m2/java
 
+    /**
+     * Optional value. Example: 11, 12, 13, 17 ...
+     * By default use the version set in project property 'maven.compiler.target'
+     */
     @Parameter(property = "javaVersion")
     Integer javaVersion; // optional
 
+    /**
+     * Optional value. You can choose an exact version, it takes precedence over the parameter 'javaVersion'.
+     * Example: jdk-16.0.1+9
+     */
     @Parameter(property = "fullJavaReleaseName")
     String fullJavaReleaseName; // optional
 
